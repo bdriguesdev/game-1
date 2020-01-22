584a5bf5c1ac13b51003e36a7407d8d952818a8a
bdriguesdev <bdriguesdev@gmail.com>
2020-01-22 16:04:47 -0300

    Depot item details improved

--- frontend/src/components/DepotContainer/DepotContainer.js
+++ frontend/src/components/DepotContainer/DepotContainer.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react';
 
 import './DepotContainer.css';
 import images from '../../utils/images';
+import statsNames from '../../utils/statsNames';
 import MainContext from '../../contexts/MainContext'
 
 const DepotContainer = props => {
@@ -82,9 +83,20 @@ const DepotContainer = props => {
             <div className={`depot-details`} hidden={isDetailsActive? false: true} id='depot-details'>
                 {
                     itemInfo && ([
-                        <p key='item-name'>{itemInfo.name}</p>,
-                        <p key='item-type'>{itemInfo.type}</p>,
-                        <p key='item-tier'>{itemInfo.tier}</p>
+                        <p className="strong" key='item-name'>T{itemInfo.tier} {itemInfo.name}</p>,
+                        <div className="detail-line" key="line-one"></div>,
+                        <p className="medium" key='item-type'>{statsNames[itemInfo.type]}</p>,
+                        itemInfo.base.map((stat, index) => {
+                            return (
+                                <p className="medium" key={"base"+index}>{statsNames[stat.stat]} +{stat.value}</p>
+                            )
+                        }),
+                        <div className="detail-line" key="line-two"></div>,
+                        itemInfo.stats.map((stat, index) => {
+                            return (
+                                <p className="light" key={"stat"+index}>{statsNames[stat.stat]} +{stat.value}</p>
+                            )
+                        }),
                     ])
                 }
             </div>
